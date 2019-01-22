3e37f03398959327b127ee572721eb5e46bd888d
Steve Belovarich <steveblue@gmail.com>
2019-01-21 19:45:30 -0800

    build script cleans directory now

--- build-lib.sh
+++ build-lib.sh
@@ -1,2 +1,11 @@
+# rollup bundles
 node_modules/.bin/rollup -c rollup.library.js
-node_modules/.bin/terser --compress --mangle --output dist/readymade.min.js -- dist/readymade.js
+
+# copy type definitions to root
+cp -R packages/@readymade/core/fesm2015/modules/. packages/@readymade/core/modules/
+
+# clean
+rm -rf packages/@readymade/core/fesm2015/modules
+rm -rf packages/@readymade/core/bundles/modules
+rm -rf packages/@readymade/core/fesm2015/app
+rm -rf packages/@readymade/core/bundles/app
