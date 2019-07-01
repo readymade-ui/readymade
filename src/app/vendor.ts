c5d7a8f768fc2c1b924d60215b1aceacd0bdb94a
Steve Belovarich <steveblue@gmail.com>
2019-06-30 23:33:34 -0700

    use yarn

--- src/app/vendor.ts
+++ src/app/vendor.ts
@@ -1,8 +1,8 @@
-export * from 'clipboard';
+// export * from 'clipboard';
 export * from 'prismjs';
 export * from 'prismjs/plugins/toolbar/prism-toolbar';
 export * from 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
-export * from 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+// export * from 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 export * from 'prismjs/components/prism-css';
 export * from 'prismjs/components/prism-javascript';
 export * from 'prismjs/components/prism-markup';
