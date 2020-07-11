913c29244b6c72dca1074bbd2f8368fc740497a7
Steve Belovarich <steveblue@gmail.com>
2020-07-10 22:19:02 -0700

    add tree test

---
+++ cypress/integration/components/tree.spec.js
@@ -0,0 +1,14 @@
+
+describe('MyTreeComponent Test', () => {
+
+  beforeEach(() => {
+    cy.visit('/test.html')
+    cy.wait(1)
+  })
+
+  it('Displays TreeComponent', () => {
+    cy.get('x-tree').should('exist')
+  })
+
+})
+
