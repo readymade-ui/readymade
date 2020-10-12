ac9a2e99069698412b96d6fedb4303b3417fcd6b
Steve Belovarich <steveblue@gmail.com>
2020-10-11 21:50:37 -0700

    feat: add tachometer

---
+++ rollup.mock.js
@@ -0,0 +1,56 @@
+import cleanup from 'rollup-plugin-cleanup';
+import commonjsResolve from '@rollup/plugin-commonjs';
+import html from 'rollup-plugin-string-html';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+
+
+const plugins = [
+    nodeResolve({
+        mainFields: ['module', 'jsnext']
+    }),
+    postcss({
+        extract: false,
+        modules: false,
+        use: [
+            ['sass', {
+                includePaths: ['src/client/style']
+            }]
+        ],
+        minimize: true,
+        extensions: ['.scss','.css']
+    }),
+    html({
+        include: ["**/*.html"],
+        exclude: ["**/index.html"],
+        minifier: {}
+    }),
+    typescript({
+        experimentalDecorators: true,
+        target: 'es5'
+    }),
+    commonjsResolve(),
+    terser(),
+    cleanup({
+        comments: 'none'
+    })
+];
+
+const onwarn = function (message) {
+    console.log(message);
+};
+
+export default [{
+    input: 'src/client/app/component/counter.ts',
+    output: {
+        name: 'counter',
+        file: 'mock/paint/index.js',
+        format: 'iife'
+    },
+    treeshake: true,
+    external: ['path', 'html-minifier-terser'],
+    plugins: plugins,
+    onwarn: onwarn
+}]
