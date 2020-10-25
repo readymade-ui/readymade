c278658b3ef703fd78aa12fc8dbc26cbd883ebd2
Steve Belovarich <steveblue@gmail.com>
2020-10-24 20:01:42 -0700

    remove packages directory

--- packages/@readymade/core/types/element/src/attach.d.ts
+++
@@ -1,6 +0,0 @@
-import { ElementMeta } from './../../decorator';
-declare function attachShadow(instance: any, options?: any): void;
-declare function attachDOM(instance: any, options?: any): void;
-declare function attachStyle(instance: any, options?: any): void;
-declare function define(instance: any, meta: ElementMeta): void;
-export { attachDOM, attachStyle, attachShadow, define };
