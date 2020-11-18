0fb4d916be24a78e5012c62e61659ed0151f40b1
Steve Belovarich <steveblue@gmail.com>
2020-11-17 23:11:07 -0800

    fix: gh pages workflow

--- pages.js
+++ pages.js
@@ -4,31 +4,27 @@ const minifier = require('htmlnano');
 const fs = require('fs');
 const path = require('path');
 
-const replace = (str) => {
- return new Promise((res,rej) => {
-   try {
-	str = str.replace(`//# sourceMappingURL=web-animations.min.js.map`, '');
-	res(str);
-   }
-   catch(err) {
-	 rej(err);
-   }
- });
-}
-
-
+const replace = str => {
+  return new Promise((res, rej) => {
+    try {
+      str = str.replace(`//# sourceMappingURL=web-animations.min.js.map`, '');
+      res(str);
+    } catch (err) {
+      rej(err);
+    }
+  });
+};
 
 (async () => {
-  const input = await fs.readFileSync('dist/index.html', 'utf8');
-   const result = posthtml([
-	 inline({ root: path.join(__dirname, 'dist')} )
-  ])
-  .process(input);
+  const input = await fs.readFileSync('dist/client/index.html', 'utf8');
+  const result = posthtml([
+    inline({ root: path.join(__dirname, 'dist', 'client') })
+  ]).process(input);
   const output = await result;
   const filtered = await replace(output.html);
-  fs.writeFile('dist/index-inline.html', filtered, (res, rej) => {
-        if (rej) {
-            process.exit();
-        }
-    });
+  fs.writeFile('dist/client/index.html', filtered, (res, rej) => {
+    if (rej) {
+      process.exit();
+    }
+  });
 })();
