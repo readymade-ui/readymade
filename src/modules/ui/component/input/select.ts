b5288f25d24f5c18c9b4788da3e73a30e335fabb
Steve Belovarich <steveblue@gmail.com>
2021-02-13 17:17:58 -0800

    feat: select styling

--- src/modules/ui/component/input/select.ts
+++ src/modules/ui/component/input/select.ts
@@ -6,14 +6,25 @@ import { SelectComponent } from './../../../dom';
   custom: { extends: 'select' },
   style: css`
     :host {
+      display: block;
       background-color: var(--color-bg);
       border: 2px solid var(--color-border);
       border-radius: 14px;
       color: var(--color-default);
       font: var(--font-family);
+      line-height: 1.3;
+      padding: 0.3em 1.4em 0.3em 0.8em;
       max-width: 280px;
-      min-height: 36px;
-      padding: 0px 14px;
+      height: 36px;
+      box-sizing: border-box;
+      margin: 0;
+      -moz-appearance: none;
+      -webkit-appearance: none;
+      appearance: none;
+      background-image: var(--icon-menu);
+      background-repeat: no-repeat;
+      background-position: right 0.7em top 50%, 0 0;
+      background-size: 10px 9px;
     }
     :host:hover,
     :host:focus,
@@ -21,6 +32,15 @@ import { SelectComponent } from './../../../dom';
       border: 2px solid var(--color-highlight);
       outline: none;
       box-shadow: none;
+    }
+    *[dir='rtl'] :host,
+    :root:lang(ar) :host,
+    :root:lang(iw) :host {
+      background-position: left 0.7em top 50%, 0 0;
+      padding: 0.3em 0.8em 0.3em 1.4em;
+    }
+    :host::-ms-expand {
+      display: none;
     }
   `
 })
