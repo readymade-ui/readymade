5eaabd2290686a50ea0b4789a18cff34b964142b
Stephen Belovarich <steveblue@gmail.com>
2024-10-24 22:55:30 -0700

    feat: hello world

---
+++ src/client/hello.ts
@@ -0,0 +1,18 @@
+import { Component, css, CustomElement, html, Listen } from '@readymade/core';
+
+@Component({
+  selector: 'hello-world',
+  style: css`
+    :host {
+      display: block;
+    }
+  `,
+  template: html` <p>Hello World</p> `,
+})
+class HelloWorldComponent extends CustomElement {
+  constructor() {
+    super();
+  }
+}
+
+export { HelloWorldComponent };
